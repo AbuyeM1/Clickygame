aa9dedd594d207a47955d456b5a61ce9673c5c10
Abuye Mamuye <abuyeye3@gmail.com>
2021-03-10 19:13:02 -0800

    creat props on nav and game js

---
+++ clickygame/src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Nav = (props) => (
+  <ul className="nav nav-fill">
+    <li className="nav-item nav-text text-left">such game.</li>
+    <li className="nav-item nav-text text-right">
+      score: {props.currentScore} | high score: {props.highScore}
+    </li>
+  </ul>
+);
+
+export default Nav;
